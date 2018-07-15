ce1a9c08480dc501a741059630acd9551bd4f204
Suganthi <suganthikrishnavathi@gmail.com>
2018-07-15 22:05:07 +1000

    Update state from test is an antipattern.
    - State is private to the component - Do not modify from test

--- examples/src/2_click/antipattern.test.js
+++ examples/src/2_click/antipattern.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import App from './App';
 import {shallow} from "enzyme";
+import Factoid from "./Factoid";
 
 describe('antipattern - click event to update count', function () {
     it('updates vote count in state', function () {
@@ -9,5 +10,14 @@ describe('antipattern - click event to update count', function () {
         wrapper.find('button').simulate('click');
 
         expect(wrapper.state().vote).toEqual(2);
+    });
+
+
+    it('updates vote count on the factoid block', function () {
+        const wrapper = shallow(<App/>);
+
+        wrapper.setState({vote: 42});
+
+        expect(wrapper.find(Factoid).props().rating).toEqual(42);
     });
 });
